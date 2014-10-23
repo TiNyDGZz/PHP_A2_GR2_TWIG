da1a3c60aa1dcbcb03b1c80bf04d5a7a5eb9562a
Adrien LOUGE <adrien.louge@gmail.com>
2014-10-23 13:09:15 +0200

    New commit

--- index.php
+++ index.php
@@ -21,6 +21,10 @@ if ($currentPage > $nbPages) {
 
 $articles = getArticles($link, null, ($currentPage-1)*$perPage, $perPage);
 
-include __DIR__.'/template/articles.php';
+echo $twig->render('article.html.twig',[
+    'articles' => $articles,
+    'connected' => true,
+    'username' => 'bleug',
+]);
 
 require __DIR__.'/_footer.php';
